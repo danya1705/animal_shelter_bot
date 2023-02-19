000aaa3caeb64635ddbe360dcb0279b010a32003
danya1705 <danya17@gmail.com>
2023-02-19 16:43:55 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course7/animal_shelter_bot/AnimalShelterBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course7.animal_shelter_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterBotApplication.class, args);
+    }
+
+}
