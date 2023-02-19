212df829000df02f5f7133a1d383b538bce96bb5
danya1705 <danya17@gmail.com>
2023-02-19 18:23:09 +0300

    initial commit

---
+++ src/test/java/pro/sky/java/course7/animal_shelter_bot/AnimalShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course7.animal_shelter_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
