3db10ceab5f88734d6d4e7649b5348e3c92e50ef
Viktor <viktor.ihtiyarov@gmail.com>
2023-02-25 18:22:31 +0700

    Внесены измененния в UserCustodian и  UserCustodianRepository

--- src/main/java/pro/sky/java/course7/animal_shelter_bot/entity/UserCustodian.java
+++ src/main/java/pro/sky/java/course7/animal_shelter_bot/entity/UserCustodian.java
@@ -1,13 +1,81 @@
 package pro.sky.java.course7.animal_shelter_bot.entity;
 
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import javax.xml.bind.annotation.XmlID;
+import java.time.LocalDateTime;
 
 
+//Сущьность, опекун животных.
+
+
+@Entity
 public class UserCustodian {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long userId;
+
+    private String fullName;
+
+    private String contacts;
+
+    private LocalDateTime custodyDate;
+
+    private String volunteer;
 
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public LocalDateTime getCustodyDate() {
+        return custodyDate;
+    }
+
+    public void setCustodyDate(LocalDateTime custodyDate) {
+        this.custodyDate = custodyDate;
+    }
+
+    public String getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(String volunteer) {
+        this.volunteer = volunteer;
+    }
 }
