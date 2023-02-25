666744287628cc8555b566b841deaf511020d3c2
Viktor <viktor.ihtiyarov@gmail.com>
2023-02-25 17:59:06 +0700

    Добавлена сущность UserCustodian и репозиторий UserCustodianRepository

---
+++ src/main/java/pro/sky/java/course7/animal_shelter_bot/entity/UserCustodian.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course7.animal_shelter_bot.entity;
+
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.xml.bind.annotation.XmlID;
+
+
+public class UserCustodian {
+
+
+
+}
