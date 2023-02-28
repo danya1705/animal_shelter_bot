5ce1af6ef370deafc55ce33e97f51f2b8e8065b8
astra <diman.lan@Mail.ru>
2023-02-28 16:58:07 +0300

    Изменены версии зависимостей для работы Swagger. Добалвен API на взаимодействие с испытательным сроком. Функционал рабочий. Изменены импорты Виктора для работы приложения. Поправлена таблица в User.sql у Виктора, добавлена своя новая.

---
+++ src/main/java/pro/sky/java/course7/animal_shelter_bot/service/TrialPeriodService.java
@@ -0,0 +1,32 @@
+package pro.sky.java.course7.animal_shelter_bot.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course7.animal_shelter_bot.entity.TrialPeriod;
+import pro.sky.java.course7.animal_shelter_bot.repository.TrialPeriodRepository;
+
+import java.util.List;
+
+@Service
+public class TrialPeriodService {
+    TrialPeriodRepository trialPeriodRepository;
+
+    public TrialPeriodService(TrialPeriodRepository trialPeriodRepository) {
+        this.trialPeriodRepository = trialPeriodRepository;
+    }
+
+    public List<TrialPeriod> findAll() {
+        return trialPeriodRepository.findAll();
+    }
+
+    public TrialPeriod createPeriod(TrialPeriod period) {
+        return trialPeriodRepository.save(period);
+    }
+
+    public TrialPeriod editTrialPeriod(TrialPeriod period) {
+        TrialPeriod findTrialPeriod = trialPeriodRepository.findById(period.getId()).orElse(null);
+        if (findTrialPeriod != null) {
+            return trialPeriodRepository.save(period);
+        }
+        return null;
+    }
+}
