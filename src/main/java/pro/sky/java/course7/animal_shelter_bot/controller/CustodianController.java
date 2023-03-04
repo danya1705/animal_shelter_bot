8ff6bd049cdc4ab72bd7eae26ee4ac430c244ea4
Sergey <zapravilsj@mail.ru>
2023-03-04 21:19:38 +0500

    Добавлены комментарии в CustodianController

--- src/main/java/pro/sky/java/course7/animal_shelter_bot/controller/CustodianController.java
+++ src/main/java/pro/sky/java/course7/animal_shelter_bot/controller/CustodianController.java
@@ -1,6 +1,11 @@
 package pro.sky.java.course7.animal_shelter_bot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.java.course7.animal_shelter_bot.model.TrialPeriod;
@@ -19,12 +24,24 @@ public class CustodianController {
         this.custodianService = custodianService;
     }
 
+
+    @Operation(summary = "Create new User Custodian", tags = "Custodian")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "New Custodian is created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserCustodian.class))})
+    })
     @PostMapping("/newCustodian")
     public ResponseEntity<UserCustodian> createCustodian(@Parameter(description = "Period to be created")
                                                          @RequestBody UserCustodian custodian) {
         return ok(custodianService.createCustodian(custodian));
     }
-
+    @Operation(summary = "Change an existed User Custodian", tags = "Custodian")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Custodian has been changed",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserCustodian.class))})
+    })
     @PutMapping("/update")
     public ResponseEntity<UserCustodian> updateCustodian(@Parameter(description = "Custodian to be created")
                                                          @RequestBody UserCustodian custodian) {
@@ -34,7 +51,12 @@ public class CustodianController {
         }
         return ResponseEntity.ok(findCustodian);
     }
-
+    @Operation(summary = "Get list of all User Custodian", tags = "Custodian")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "There's the list",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserCustodian.class))})
+    })
     @GetMapping("/getAllCustodian")
     public ResponseEntity<List<UserCustodian>> getCustodian() {
         List<UserCustodian> foundCustodian = custodianService.findAll();
